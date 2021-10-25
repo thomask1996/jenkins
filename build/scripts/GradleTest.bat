0285b6a706739aa6ea2fba3a70a0eabd21ecd616
thomask1996 <thomas.katz@wxlibrisgroup.com>
2021-10-25 15:30:19 +0300

    hello

---
+++ build/scripts/GradleTest.bat
@@ -0,0 +1,89 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  GradleTest startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%..
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_TEST_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\lib\GradleTest-1.0-SNAPSHOT.jar;%APP_HOME%\lib\java-client-7.3.0.jar;%APP_HOME%\lib\selenium-java-3.141.59.jar;%APP_HOME%\lib\selenium-chrome-driver-3.141.59.jar;%APP_HOME%\lib\selenium-edge-driver-3.141.59.jar;%APP_HOME%\lib\selenium-firefox-driver-3.141.59.jar;%APP_HOME%\lib\selenium-ie-driver-3.141.59.jar;%APP_HOME%\lib\selenium-opera-driver-3.141.59.jar;%APP_HOME%\lib\selenium-safari-driver-3.141.59.jar;%APP_HOME%\lib\selenium-support-3.141.59.jar;%APP_HOME%\lib\selenium-remote-driver-3.141.59.jar;%APP_HOME%\lib\selenium-api-3.141.59.jar;%APP_HOME%\lib\byte-buddy-1.8.15.jar;%APP_HOME%\lib\commons-exec-1.3.jar;%APP_HOME%\lib\guava-25.0-jre.jar;%APP_HOME%\lib\okhttp-3.11.0.jar;%APP_HOME%\lib\okio-1.14.0.jar;%APP_HOME%\lib\gson-2.8.5.jar;%APP_HOME%\lib\httpclient-4.5.9.jar;%APP_HOME%\lib\cglib-3.2.12.jar;%APP_HOME%\lib\commons-validator-1.6.jar;%APP_HOME%\lib\commons-lang3-3.9.jar;%APP_HOME%\lib\commons-io-2.6.jar;%APP_HOME%\lib\spring-context-5.1.8.RELEASE.jar;%APP_HOME%\lib\aspectjweaver-1.9.4.jar;%APP_HOME%\lib\slf4j-api-1.7.26.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\checker-compat-qual-2.0.0.jar;%APP_HOME%\lib\error_prone_annotations-2.1.3.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar;%APP_HOME%\lib\httpcore-4.4.11.jar;%APP_HOME%\lib\commons-beanutils-1.9.2.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.11.jar;%APP_HOME%\lib\asm-7.1.jar;%APP_HOME%\lib\commons-digester-1.8.1.jar;%APP_HOME%\lib\commons-collections-3.2.2.jar;%APP_HOME%\lib\spring-aop-5.1.8.RELEASE.jar;%APP_HOME%\lib\spring-beans-5.1.8.RELEASE.jar;%APP_HOME%\lib\spring-expression-5.1.8.RELEASE.jar;%APP_HOME%\lib\spring-core-5.1.8.RELEASE.jar;%APP_HOME%\lib\spring-jcl-5.1.8.RELEASE.jar
+
+
+@rem Execute GradleTest
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_TEST_OPTS%  -classpath "%CLASSPATH%"  %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_TEST_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%GRADLE_TEST_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
---
+++ src/main/java/Appium.java
@@ -0,0 +1,142 @@
+//import io.appium.java_client.MobileElement;
+//import io.appium.java_client.android.AndroidDriver;
+//
+////import org.junit.AfterClass;
+////import org.junit.BeforeClass;
+////import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//import java.util.Objects;
+//import java.util.Set;
+//import java.util.concurrent.TimeUnit;
+//
+//public class Appium {
+//    static AndroidDriver<MobileElement> driver;
+//    static WebDriverWait wait;
+//
+//
+//    @BeforeClass
+//    public static void enterApp() throws MalformedURLException {
+//        //set up
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        // local appium
+////        capabilities.setCapability("deviceName", "Auto");
+////        capabilities.setCapability("platformName", "Android");
+////        capabilities.setCapability("app", "C:\\Users\\thomask\\Downloads\\QA2-master-2222-Debug.apk");
+////        capabilities.setCapability("appActivity", "com.ombiel.campusm.startup.Main");
+////        capabilities.setCapability("automationName", "UiAutomator2");
+////        capabilities.setCapability("autoGrantPermissions", true);
+////        URL url = new URL("http://localhost:4723/wd/hub");
+////        driver = new AndroidDriver<MobileElement>(url ,capabilities);
+//        //Perfecto appium
+//        String securityToken = "eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzMTU4OWQ5OS01ZGJjLTQzMDYtYWE5OC03YzZkNzBhZDJjMWIifQ.eyJpYXQiOjE2MzM1ODk3Mjks" +
+//                "Imp0aSI6IjgwOGFmNGY0LTQ5MDctNDMzZC05NzU1LThiYWU1ZDI3Nzg2ZCIsImlzcyI6Imh0dHBzOi8vYXV0aDQucGVyZmVjdG9tb2JpbGUuY29tL2F1dGgvcmVhbG1zL2NhbXB1c20tc" +
+//                "HVibGljLXBlcmZlY3RvbW9iaWxlLWNvbSIsImF1ZCI6Imh0dHBzOi8vYXV0aDQucGVyZmVjdG9tb2JpbGUuY29tL2F1dGgvcmVhbG1zL2NhbXB1c20tcHVibGljLXBlcmZlY3RvbW9ia" +
+//                "WxlLWNvbSIsInN1YiI6ImY2ZDI0MDlkLThlOTAtNGE1Yi1iYzBkLTY0NDczMmNhZDM2ZSIsInR5cCI6Ik9mZmxpbmUiLCJhenAiOiJvZmZsaW5lLXRva2VuLWdlbmVyYXRvciIsIm5vb" +
+//                "mNlIjoiOWZiMzY5OGQtNTE4NC00M2RkLThlNTUtYzMzZTI4YzI1MDJkIiwic2Vzc2lvbl9zdGF0ZSI6IjljMmMzYjliLWY1ZDQtNGU2Yi1hNWFkLThiMjFhZTFmMTQzMiIsInNjb3BlI" +
+//                "joib3BlbmlkIHByb2ZpbGUgb2ZmbGluZV9hY2Nlc3MgZW1haWwifQ.ACW2Pot_eIuKj4_ycdyCS_GrqVhbTFA_vg4jmue2dAo";
+//        capabilities.setCapability("securityToken", securityToken);
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("model", "Galaxy S.*|LG.*");
+//        capabilities.setCapability("app", "PUBLIC:QA2-2222-Android.apk");
+//        capabilities.setCapability("appPackage", "com.ombiel.campusm.autoqa22222beta");
+//        capabilities.setCapability("autoLaunch", true); // Whether to install and launch the app automatically.
+//        capabilities.setCapability("takesScreenshot", false);
+//        capabilities.setCapability("screenshotOnError", true);
+//        capabilities.setCapability("openDeviceTimeout", 5);
+//        capabilities.setCapability("autoGrantPermissions", true);
+//        capabilities.setCapability("enableAppiumBehavior", true);
+//        capabilities.setCapability("useAppiumForHybrid", true);
+//
+//        String cloudName = "campusm-public";
+//        driver = new AndroidDriver<>(new URL("https://" + cloudName.replace(".perfectomobile.com", "")
+//                + ".perfectomobile.com/nexperience/perfectomobile/wd/hub"), capabilities);
+//        wait = new WebDriverWait(driver, 45);
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//        //login s
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout[5]/android.widget.TextView")).click();
+//        driver.findElement(By.id("com.ombiel.campusm.manual2222beta:id/acceptcontactContainer")).click();
+//        driver.findElement(By.id("com.ombiel.campusm.manual2222beta:id/email")).sendKeys("hannan");
+//        driver.findElement(By.id("com.ombiel.campusm.manual2222beta:id/password")).sendKeys("campusm");
+//        driver.findElement(By.id("com.ombiel.campusm.manual2222beta:id/login")).click();
+//        // login thomas
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget." +
+//                "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget." +
+//                "ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget." +
+//                "TextView")).click();
+//        //skip fingerprint
+////        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.ombiel.campusm.manual2222beta:id/btnSkipBiometrics")));
+////        driver.findElement(By.id("com.ombiel.campusm.manual2222beta:id/btnSkipBiometrics")).click();
+//    }
+//
+//    @Test
+//    public void checkSite() throws InterruptedException {
+//        //enter site
+//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"android#\"]/android.view.View")).click();
+//        //accept cookies
+//
+////            driver.findElement(By.id("fcbarcelona-button-accept")).click();
+//        driver.context("WEBVIEW_com.ombiel.campusm.manual2222beta");
+////        driver.context("VISUAL");
+////        System.out.println(driver.getPageSource());
+//        Set<String> handles = driver.getWindowHandles();
+//        String winHandle = driver.getWindowHandle();
+//        handles.remove(winHandle);
+//        String winHandle1 = handles.iterator().next();
+//        if (!Objects.equals(winHandle, winHandle1)) {
+//            driver.switchTo().window(winHandle1);
+//        }
+//        try {
+//            driver.findElement(By.xpath("//*[@id=\"close_DFP_href\"]/img")).click();
+//        }
+//        catch (Exception e){
+//            System.out.println("Banner never showed");
+//        }
+//        try {
+//            driver.findElement(By.xpath("//*[@id=\"fcbarcelona-button-accept\"]")).click();
+//        }
+//        catch (Exception e){
+//            System.out.println("Cookies already accepted");
+//        }
+////        driver.findElement(By.id("fcbarcelona-button-accept")).click();
+//        Thread.sleep(2000);
+//        String title = driver.getTitle();
+//        if (title.equals("Official FC Barcelona Website")) {
+//            System.out.println("Title is ok: " + title);
+//        } else {
+//            System.out.println("Wrong title, the title was found is " + title);
+//        }
+//        // back to app
+//        driver.context("NATIVE_APP");
+//        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+//    }
+//
+//    @Test
+//    public void checkPoll() throws InterruptedException {
+//        //enter poll
+//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"Poll Of The Week\"]")).click();
+//        //choose poll
+//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"Choose Answer poa 06 October 2021\"]/android.view.View[3]/android.widget.Image")).click();
+//        //choose answer and confirm
+//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"1\"]/android.view.View[1]/android.widget.Image")).click();
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget." +
+//                "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/androidx." +
+//                "slidingpanelayout.widget.SlidingPaneLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android." +
+//                "widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android." +
+//                "view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button")).click();
+//        //check results
+//        driver.findElement(By.xpath("//android.view.View[@content-desc=\"Show Quick Poll Results\"]")).click();
+//        Thread.sleep(2000);
+//        // back to app
+//        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]")).click();
+//    }
+//
+//    @AfterClass
+//    public static void quit(){
+//        driver.quit();
+//    }
+//}
